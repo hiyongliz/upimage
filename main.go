8d7465de0184f91221a47dc725c10771f05da926
hiyongliz <644220198@qq.com>
2025-07-17 11:14:28 +0800

    refactor: remove version information variables from main.go

--- main.go
+++ main.go
@@ -7,13 +7,6 @@ import (
 	"github.com/hiyongliz/upimage/cmd"
 )
 
-// Version information set by build flags
-var (
-	version   = "dev"
-	buildTime = "unknown"
-	commitSHA = "unknown"
-)
-
 func main() {
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
